7c1dd30fbc946d248427f8b456d03be2d312ad17
rohilrajkarki <rohilrajkarki@gmail.com>
2022-08-16 10:12:58 +0545

    detials page

--- src/feature/Slices/movieSlice.js
+++ src/feature/Slices/movieSlice.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 const initialState = {
     movies: {},
+    selectedMovie: {}
 }
 
 export const fetchMoviesAsync = createAsyncThunk(
@@ -12,7 +13,13 @@ export const fetchMoviesAsync = createAsyncThunk(
         const movieText = "mystery";
         return await axios.get(`?apikey=${APIKEY}&s=${movieText}&type=movie&page=${page}`)
             .then(res => res.data)
-        // return resp.data
+    })
+
+export const fetchMoviesDetailsAsync = createAsyncThunk(
+    'movie/fetchMoviesDetailsAsync',
+    async (id) => {
+        return await axios.get(`?apikey=${APIKEY}&i=${id}&Plot=full`)
+            .then(res => res.data)
     })
 
 const movieSlice = createSlice({
@@ -29,7 +36,7 @@ const movieSlice = createSlice({
                 console.log("pending")
             })
             builder.addCase(fetchMoviesAsync.fulfilled, (state, { payload }) => {
-                console.log("Movies Fetched")
+                // console.log("Movies Fetched")
                 state.movies = payload
                 // console.log(state.movies)
 
@@ -37,11 +44,16 @@ const movieSlice = createSlice({
             builder.addCase(fetchMoviesAsync.rejected, () => {
                 console.log("rejected")
             })
+            builder.addCase(fetchMoviesDetailsAsync.fulfilled, (state, { payload }) => {
+                state.selectedMovie = payload
+
+            })
         }
 
 })
 
 export const { addMovies } = movieSlice.actions;
 //store bata value leko
-export const getAllMovies = (state) => state.movies.movies//state.slicename.property
+export const getAllMovies = (state) => state.movies.movies//state.reducername.property
+export const getMoviesDetail = (state) => state.movies.selectedMovie
 export default movieSlice.reducer
